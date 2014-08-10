b459feef79494091b3bec2ee54456cc76819ec12
David Rosile <drosile@gmail.com>
2014-08-10 17:12:05 -0400

    display owner with cats

--- models/cat.rb
+++ models/cat.rb
@@ -5,6 +5,22 @@ class Cat < Sequel::Model
 
   set_allowed_columns :name, :description, :image_url, :gender, :birthday, :user_id
 
+  def to_hash
+    { id: id,
+      name: name,
+      description: description,
+      image_url: image_url,
+      gender: gender,
+      birthday: birthday,
+      owner: { username: user.username,
+               name: user.name }
+    }
+  end
+
+  def to_json(_options = {})
+    to_hash.to_json
+  end
+
   private
 
   def validate
