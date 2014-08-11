5e6c9825051420efd3bd2367728c67afbbf71ecf
David Rosile <drosile@gmail.com>
2014-08-11 01:57:25 -0400

    refactor user api routes

--- routes/api/users.rb
+++ routes/api/users.rb
@@ -3,6 +3,20 @@ require_relative '../api'
 class UsersAPIRoutes < APIRoutes
   #/api/users
   route do |r|
+    r.get true do
+      current_user || unauthorized!
+      current_user.role == 'admin' || forbidden!
+      halt 200, body: { users: User.all }
+    end
+
+    r.post true do
+      if params[:role] != 'user'
+        current_user || unauthorized!
+        current_user.role == 'admin' || forbidden!
+      end
+      create! User, params
+    end
+
     r.post 'authenticate' do
       user = User.authenticate(params[:identifier], params[:password])
 
@@ -28,44 +42,20 @@ class UsersAPIRoutes < APIRoutes
       (current_user.id == user_id.to_i) || (current_user.role == 'admin') || forbidden!
       @user = User[user_id] || not_found!
 
-      r.get do
-        r.is do
-          read! @user
-        end
+      r.get true do
+        read! @user
       end
 
-      r.put do
-        r.is do
-          if params[:role] == 'admin'
-            current_user || unauthorized!
-            current_user.role == 'admin' || forbidden!
-          end
-          update! User, @user.id, params
-        end
-      end
-
-      r.delete do
-        r.is do
-          destroy! User, @user.id
-        end
-      end
-    end
-
-    r.get do
-      r.is do
-        current_user || unauthorized!
-        current_user.role == 'admin' || forbidden!
-        halt 200, body: { users: User.all }
-      end
-    end
-
-    r.post do
-      r.is do
-        if params[:role] != 'user'
+      r.put true do
+        if params[:role] == 'admin'
           current_user || unauthorized!
           current_user.role == 'admin' || forbidden!
         end
-        create! User, params
+        update! User, @user.id, params
+      end
+
+      r.delete true do
+        destroy! User, @user.id
       end
     end
   end
